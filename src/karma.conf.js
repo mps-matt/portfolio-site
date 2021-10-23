d56cc5fa41be1a362f2450284ab48bca3f2a522b
mps-matt <93010094+mps-matt@users.noreply.github.com>
2021-10-23 11:34:17 +0100

    fixing eslint to not fail on using import export minor fix

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,22 +1,18 @@
 // karma.conf.js
-module.exports = function(config) {
-  config.set({
-    basePath: '../..',
-    frameworks: ['jasmine'],
-    files: [
-      'app/**/*.spec.js',
-      'app/**/*.spec.ts'
-    ],
-    exclude: [
-    ],
-    preprocessors: {},
-    reporters: [],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['ChromeHeadless'],
-    singleRun: true,
-    failOnEmptyTestSuite: false
-  });
+module.exports = function (config) {
+	config.set({
+		basePath: '../..',
+		frameworks: ['jasmine'],
+		files: ['app/*.spec.js', 'app/**/*.spec.js', 'app/**/*.spec.ts'],
+		exclude: [],
+		preprocessors: {},
+		reporters: [],
+		port: 9876,
+		colors: true,
+		logLevel: config.LOG_INFO,
+		autoWatch: true,
+		browsers: ['ChromeHeadless'],
+		singleRun: true,
+		failOnEmptyTestSuite: true
+	});
 };
