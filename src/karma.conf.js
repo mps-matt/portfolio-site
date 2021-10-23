a2e19e4455c578ffeebc0d45a29d4fe9bf2835fe
mps-matt <93010094+mps-matt@users.noreply.github.com>
2021-10-23 12:35:25 +0100

    minor

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -3,9 +3,15 @@ module.exports = function (config) {
 	config.set({
 		basePath: '../..',
 		frameworks: ['jasmine', 'karma-typescript'],
-		files: [{ pattern: '**/src/**/*.spec.ts', type: 'js' }],
+		files: [
+			{ pattern: '**/src/app/*.component.ts', type: 'js' },
+			{ pattern: '**/src/app/*.module.ts', type: 'js' },
+			{ pattern: '**/src/app/*.spec.ts', type: 'js' }
+		],
 		exclude: ['node_modules'],
 		preprocessors: {
+			'**/src/app/**/*.component.ts': 'karma-typescript',
+			'**/src/app/**/*.module.ts': 'karma-typscript',
 			'**/src/app/**/*.spec.ts': 'karma-typescript'
 		},
 		reporters: ['progress', 'karma-typescript'],
