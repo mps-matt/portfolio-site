8c17cb5c71d2761e362d60057ddedb294955d415
MPS-Matt <mps.main+github@protonmail.com>
2021-10-29 20:51:04 +0000

    adding new files for the new portfolio data service

---
+++ src/app/Interfaces/iportfolio-service.ts
@@ -0,0 +1,3 @@
+export interface IPortfolioService {
+    getBannerImage: string;
+}
---
+++ src/app/portfolio-service/in-memory-portfolio-service/in-memory-portfolio-service.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { InMemoryPortfolioServiceService } from './in-memory-portfolio-service.service';
+
+describe( 'InMemoryPortfolioServiceService', () => {
+    let service: InMemoryPortfolioServiceService;
+
+    beforeEach( () => {
+        TestBed.configureTestingModule( {} );
+        service = TestBed.inject( InMemoryPortfolioServiceService );
+    } );
+
+    it( 'should be created', () => {
+        expect( service ).toBeTruthy();
+    } );
+} );
