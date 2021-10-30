56e9c554e9b9dc34fc218c72f36a28f9abeae4c3
MPS-Matt <mps.main+github@protonmail.com>
2021-10-30 23:17:01 +0000

    added material library and added an quick mock up of the linked in component to the center of the home banner component

--- src/app/linkedin/linkedin.module.ts
+++ src/app/linkedin/linkedin.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { LinkedinComponent } from './linkedin.component';
 
 @NgModule({
     declarations: [
         LinkedinComponent
+    ],
+    imports: [
+        MatCardModule
     ],
     exports: [
         LinkedinComponent
