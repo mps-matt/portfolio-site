012fe8167116b3121cb740c5474dc3097a49c069
MPS-Matt <mps.main+github@protonmail.com>
2021-10-31 18:34:43 +0000

    added linkedin icon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
     declarations: [
@@ -14,8 +13,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
         BrowserModule,
         AppRoutingModule,
         HomeModule,
-        BrowserAnimationsModule,
-        FontAwesomeModule
+        BrowserAnimationsModule
     ],
     bootstrap: [ AppComponent ]
 })
--- src/app/linkedin/linkedin.component.ts
+++ src/app/linkedin/linkedin.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
     selector: 'app-linkedin',
@@ -6,6 +7,6 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: [ './linkedin.component.css' ]
 })
 export class LinkedinComponent {
-
+    faLinkedin = faLinkedin;
     constructor() { }
 }
