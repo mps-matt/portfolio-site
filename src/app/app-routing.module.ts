eb30c2bcb1e9dbc16e1b3260320985fff27ac596
MPS-Matt <mps.main+github@protonmail.com>
2021-11-13 23:34:19 +0000

    fixed the redirect routing issue by ensuring that default is the home page and not /home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'home', component: HomePageComponent}
+    { path: '', component: HomePageComponent }
 ];
 
 @NgModule({
