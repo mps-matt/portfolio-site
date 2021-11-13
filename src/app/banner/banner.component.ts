8fe12c64434377efd6fc035a2b50160b1cae683d
MPS-Matt <mps.main+github@protonmail.com>
2021-11-13 21:40:52 +0000

    stopped page from being zoomable and added parralax library to make things simple, made the linkedin bar and the following white bar scroll faster to give a sense of seperation between the objects

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IPortfolioService } from '../portfolio-service/iportfolio-service';
+
+declare let Rellax : any;
 
 @Component({
     selector: 'app-banner',
     templateUrl: './banner.component.html',
     styleUrls: [ './banner.component.css' ]
 })
-export class BannerComponent {
+export class BannerComponent implements OnInit {
 
     constructor(
     public readonly portfolioService: IPortfolioService
     ) { }
+
+    ngOnInit() {
+        const rellax = new Rellax( '.rellax' );
+    }
 }
